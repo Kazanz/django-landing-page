d6521930c57edffe2df1d9ecaf5842974f93d359
Zach Kazanski <kazanski.zachary@gmail.com>
2015-01-30 22:34:49 -0500

    Removed extra files

---
+++ liloli/local_settings.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'gg28fhzhhH*#(38H8#*(gf#(@#F)HHFfGFG23hi3)Gnv'
+
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+DEBUG_APPS = (
+    'django_extensions',
+    'django_nose',
+)
+
+PIPELINE_VERSION = False
