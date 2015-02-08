51b64cef0d74f58a09cd145dab65d7865fbac512
Zach Kazanski <kazanski.zachary@gmail.com>
2015-02-08 13:12:26 -0500

    Shoud work in production

--- liloli/local_settings.py
+++ liloli/local_settings.py
@@ -2,7 +2,7 @@ SECRET_KEY = 'gg28fhzhhH*#(38H8#*(gf#(@#F)HHFfGFG23hi3)Gnv'
 
 DEBUG = TEMPLATE_DEBUG = True
 
-# DEBUG_APPS = (
-#     'django_extensions',
-#     'django_nose',
-# )
+DEBUG_APPS = (
+    'django_extensions',
+    'django_nose',
+)
