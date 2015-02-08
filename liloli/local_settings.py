d47ac28131a0e6420b0565a6324294e0c02ce773
root <root@server.lilolipretzels.com>
2015-02-08 20:00:13 +0000

    Working in production

--- liloli/local_settings.py
+++ liloli/local_settings.py
@@ -1,8 +1,3 @@
-SECRET_KEY = 'gg28fhzhhH*#(38H8#*(gf#(@#F)HHFfGFG23hi3)Gnv'
+SECRET_KEY = 'fheglkwehgwkghiohwg)Gnv'
 
-DEBUG = TEMPLATE_DEBUG = True
-
-DEBUG_APPS = (
-    'django_extensions',
-    'django_nose',
-)
+DEBUG = True
